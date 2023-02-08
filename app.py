83e608fc8e10f04600a2e4c4f00c4990e81d84fa
Sravani Charugundla <97333532+beginner-me@users.noreply.github.com>
2023-02-08 10:25:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,74 +1,72 @@
-
-# 1. Library imports
 import uvicorn
+import gunicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
 import numpy as np
 import pickle
 import pandas as pd
+app=FastAPI()
+pickle_in1=open('clsxgb.pkl','rb')
+classifier1=pickle.load(pickle_in1)
 
-# 2. Create the app object
-app = FastAPI()
-pickle_in = open("./static/random-forest-water.sav", "rb")
-classifier = pickle.load(pickle_in)
+pickle_in2=open('clsrf.pkl','rb')
+classifier2=pickle.load(pickle_in2)
 
+pickle_in3=open('clslr.pkl','rb')
+classifier3=pickle.load(pickle_in3)
 
-# 3. Index route, opens automatically on http://127.0.0.1:8000
+def predicts(df,num):
+    if num==1:
+        return classifier1.predict(df)
+    if num==2:    
+        return classifier2.predict(df)
+    if num==3:
+       return classifier3.predict(df)  
+    
+
+class Water(BaseModel):
+    ph:float
+    Hardness:float
+    Solids:float
+    Chloramines:float
+    Sulfate:float
+    Conductivity:float
+    Organic_carbon:float
+    Trihalomethanes:float
+    Turbidity:float
+
 @app.get('/')
 def index():
-    return {'message': 'Welcome to water quality prediction API'}
-
-@app.get('/inputs')
-def Inputs_to_api():
-    return {"Provide json data for ph,Hardness,Solids,Chloramines,Sulfate,Conductivity,Organic_carbon,Trihalomethanes,Turbidity"}
-
-
-
-# 4. Route with a single parameter, returns the parameter within a message
-#    Located at: http://127.0.0.1:8000/AnyNameHere
-class Pollutants(BaseModel):
-    ph: float
-    Hardness: float
-    Solids: float
-    Chloramines: float
-    Sulfate: float
-    Conductivity: float
-    Organic_carbon: float
-    Trihalomethanes: float
-    Turbidity: float
-
-# 3. Expose the prediction functionality, make a prediction from the passed
-#    JSON data and return the predicted Bank Note with the confidence
+    return{'message': 'Welcome to water quality prediction API'}
+@app.get('/name')
+def get_name(name:str):
+    return{'message':f'Hello,{name}'}    
 @app.post('/predict')
-def Predict_level_of_pollution(data: Pollutants):
+# def get_model(model: int):
+#     return model
+def predict_water(data:Water,model:int):
+    data=data.dict()
     print(data)
-    data = data.dict()
-    print(data)
-    ph = data['ph']
-    Hardness = data['Hardness']
-    Solids = data['Solids']
-    Chloramines = data['Chloramines']
-    Sulfate = data['Sulfate']
-    Conductivity = data['Conductivity']
-    Organic_carbon = data['Organic_carbon']
-    Trihalomethanes = data['Trihalomethanes']
-    Turbidity = data['Turbidity']
-
-
-    prediction = classifier.predict([[ph,Hardness,Solids,Chloramines,Sulfate,Conductivity,Organic_carbon,Trihalomethanes,Turbidity]])
-    prediction = prediction.tolist()
-    return {
-        'potability': prediction[0],
-        'Result' : 'Potable' if prediction[0]==1 else 'Not Potable'
+    ph=data['ph']
+    Hardness=data['Hardness']
+    Solids=data['Solids']
+    Chloramines=data['Chloramines']
+    Sulfate=data['Sulfate']
+    Conductivity=data['Sulfate']
+    Organic_carbon=data['Organic_carbon']
+    Trihalomethanes=data['Trihalomethanes']
+    Turbidity=data['Turbidity']
+    values = [[ph,Hardness,Solids,Chloramines,Sulfate,Conductivity,Organic_carbon,Trihalomethanes,Turbidity]]
+    df = pd.DataFrame(values, columns=["ph","Hardness","Solids","Chloramines","Sulfate","Conductivity","Organic_carbon","Trihalomethanes","Turbidity"])
+    # prediction=classifier.predict(df)
+    prediction=predicts(df,model)
+    if(prediction[0]>0.5):
+        prediction="Safe to drink"
+    else:
+        prediction="Unsafe to drink"
+    return{
+        'prediction':prediction
     }
-# @app.get('/{name}')
-# def get_name(name: str):
-#     return {'Welcome To Krish Youtube Channel': f'{name}'}
-
-
-# 5. Run the API with uvicorn
-# #    Will run on http://127.0.0.1:8000
-# if __name__ == '__main__':
-#     uvicorn.run(app, host='127.0.0.1', port=8000)
-
-# uvicorn app:app --reload
+    # if _name=='main_':
+    #     uvicorn.run(app,host='126.0.0.1',port=8000)
+#uvicorn app:app --reload
